a366f0c75134b4ab98bc6233338a6141a50cbf28
Jonghwa Seo <pincoins@gmail.com>
2019-07-28 20:46:28 +0700

    posix type pid_t

--- sandbox.pyx
+++ sandbox.pyx
@@ -1,4 +1,9 @@
+from posix.types cimport pid_t
+
 cdef extern from "judge.h":
+    int examine(int argc, char *argv[])
+    void run_solution(char **args)
+    void watch_program(pid_t pid)
     int hello(int i, char c, char *s)
 
 def py_hello(i, c, s):
