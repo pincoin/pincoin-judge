87de512e1b1a82a64fd178d4d5930a9d06ae3546
Jonghwa Seo <pincoins@gmail.com>
2019-07-28 23:55:40 +0700

    cython

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import sandbox
 
 if __name__ == '__main__':
     sandbox.py_hello(3, ord('c'), b'world')
+    sandbox.py_examine(['/bin/date', ])
 
--- sandbox.pyx
+++ sandbox.pyx
@@ -1,9 +1,25 @@
+from libc.stdlib cimport malloc, free
 from posix.types cimport pid_t
 
 cdef extern from "judge.h":
     int examine(int argc, char *argv[])
 
     int hello(int i, char c, char *s)
+
+
+def py_examine(args):
+    cdef char **argv
+
+    args = [b'a.out'] + [bytes(x, encoding='utf-8') for x in args]
+    argv = <char**>malloc(sizeof(char*) * len(args))
+
+    for i, s in enumerate(args):
+        argv[i] = s
+
+    examine(len(args), argv)
+
+    free(argv)
+
 
 def py_hello(i, c, s):
     print(hello(i, c, s))
