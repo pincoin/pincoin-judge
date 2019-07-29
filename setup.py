cfa29247bfe6336349de7a2667aba85be116acf9
Jonghwa Seo <pincoins@gmail.com>
2019-07-29 17:41:12 +0700

    language level

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 sandbox_extension = Extension(
-    name="sandbox",
-    sources=["sandbox.pyx"],
-    libraries=["sandbox", "seccomp"],
-    library_dirs=["lib"],
-    include_dirs=["lib"]
+    name='sandbox',
+    sources=['sandbox.pyx'],
+    libraries=['sandbox', 'seccomp'],
+    library_dirs=['lib'],
+    include_dirs=['lib']
 )
 
 setup(
-    name="pincoin sandbox",
-    ext_modules=cythonize([sandbox_extension])
+    name='pincoin sandbox',
+    ext_modules=cythonize([
+        sandbox_extension,
+    ], compiler_directives={'language_level' : '3'})
 )
